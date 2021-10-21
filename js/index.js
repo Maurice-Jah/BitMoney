1a3714ab2129521054006f17775d49ffc825489c
mauricebuchi <mauricejahwizwid@gmail.com>
2021-10-21 11:50:23 +0100

    add javascript to tabs

--- js/index.js
+++ js/index.js
@@ -5,3 +5,47 @@ for (i = 0; i < accordion.length; i++) {
     this.classList.toggle("active");
   });
 }
+
+// TABS
+var x = document.getElementById("buy");
+var y = document.getElementById("sell");
+var z = document.getElementById("trade");
+var xLabel = document.getElementById("buy_id");
+var yLabel = document.getElementById("sell_id");
+var zLabel = document.getElementById("trade_id");
+
+const hide = (element) => {
+  element.classList.add("hidden");
+};
+const inactive = (element) => {
+  element.classList.remove("tab-active");
+};
+const active = (element) => {
+  element.classList.add("tab-active");
+};
+
+function showBuy() {
+  x.classList.toggle("hidden");
+  active(xLabel);
+  hide(y);
+  inactive(yLabel);
+  hide(z);
+  inactive(zLabel);
+}
+function showTrade() {
+  z.classList.toggle("hidden");
+  active(zLabel);
+  hide(x);
+  inactive(xLabel);
+  hide(y);
+  inactive(yLabel);
+}
+
+function showSell() {
+  y.classList.toggle("hidden");
+  active(yLabel);
+  hide(x);
+  inactive(xLabel);
+  hide(z);
+  inactive(zLabel);
+}
